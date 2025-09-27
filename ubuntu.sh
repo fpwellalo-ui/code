b2a49c329f44f69f33cad9e997b4b4fb30802f40
fpwellalo-ui <fpwellalo@gmail.com>
2025-09-27 15:52:55 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 apt update
-apt install screen -y
-apt install zmap -y 
-apt install gcc gcc-c++ -y 
+apt install -y screen zmap gcc g++
 gcc src/*.c -o android -s -Os -lpthread
 screen -S scan -dm sh scan.sh
